f2bc5dd4ddef814ebfedb139eec497df40c8a0c9
Sandqvist <peter.sandqvist@sogeti.com>
2019-10-03 15:44:51 +0200

    added react router and get single user

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
