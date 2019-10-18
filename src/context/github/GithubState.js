42d18cc02a5c61095920bab692d4264c65d383d9
Sandqvist <peter.sandqvist@sogeti.com>
2019-10-18 16:55:11 +0200

    Moving User state to Context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,14 +35,24 @@ const GithubState = props => {
   };
 
   // Get user
+  const getUser = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
+  //Get single Github user
 
   // Get repos
 
   // Clear users
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   // Set loading
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -54,7 +64,9 @@ const GithubState = props => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
-        searchUsers
+        searchUsers,
+        clearUsers,
+        getUser
       }}
     >
       {props.children}
