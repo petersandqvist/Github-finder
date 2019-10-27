910cd65a76bb1e73e27f5f7510977f1413ea0802
Sandqvist <peter.sandqvist@sogeti.com>
2019-10-27 15:48:08 +0100

    repos state to context, alert context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -49,8 +49,19 @@ const GithubState = props => {
   };
   //Get single Github user
 
-  // Get repos
+  //Get users repos
+  const getUserRepos = async username => {
+    setLoading(true);
 
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
   // Clear users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
@@ -66,7 +77,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
